736d64c24b4c0ca5bc7437383f88e598fb200834
francivaldo4334 <francivaldodev@gmail.com>
2023-09-20 14:56:45 -0300

    feat:

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const listImageContainer = document.getElementById('image-list');
-    fetch('https://api.github.com/users/francivaldo4334/repos?type=public&sort=updated&per_page=10')
+
+const prevButton = document.getElementById('prev-btn');
+const nextButton = document.getElementById('next-btn');
+const listImageContainer = document.getElementById('image-list');
+let currentPage = 1;
+let totalPages = 0;
+let maxItenPage = 6;
+function getListByPage(page){
+    if(window.innerWidth < 769){
+        maxItenPage = 30
+    }
+    else{
+        maxItenPage = 6
+    }
+    var rote = 'https://api.github.com/users/francivaldo4334/repos?type=public&sort=updated&per_page='+maxItenPage+'&page='+page
+    fetch(rote)
         .then(response => {
             if (!response.ok) {
                 throw new Error('Erro ao obter os repositórios do usuário');
@@ -8,6 +21,7 @@ document.addEventListener('DOMContentLoaded', function() {
             return response.json();
         })
         .then(data => {
+            listImageContainer.innerHTML = ''
             data.forEach(repo => {
                 var url = 'https://github-readme-stats.vercel.app/api/pin/?username=francivaldo4334&repo='+repo.name;
                 const itemImg = document.createElement('li');
@@ -23,4 +37,44 @@ document.addEventListener('DOMContentLoaded', function() {
             });
         })
         .catch(error => console.error('Erro:', error));
-  });
+}
+function loadTotalPage(){
+    if(window.innerWidth < 769){
+        maxItenPage = 10
+    }
+    else{
+        maxItenPage = 6
+    }
+    document.addEventListener('DOMContentLoaded', function(){
+        fetch('https://api.github.com/users/francivaldo4334')
+            .then(response =>{
+                if(!response.ok){
+                    return new Error('Erro get total page')
+                }
+                return response.json();
+            })
+            .then(data =>{
+                totalPages = data.public_repos/maxItenPage
+            })
+    })
+}
+function setupPagination() {
+    prevButton.addEventListener('click', () => {
+      if (currentPage > 1) {
+        currentPage--;
+        getListByPage(currentPage);
+      }
+      console.log(currentPage);
+    });
+  
+    nextButton.addEventListener('click', () => {
+        if (currentPage < totalPages) {
+            currentPage++;
+            getListByPage(currentPage);
+        }
+        console.log(currentPage);
+    });
+  }
+loadTotalPage()
+getListByPage(1)
+setupPagination()
